e7f8cf93574a46dad478ddff4871ac05e045c5fe
Filippe Braga <filippeffx@hotmail.com>
2023-04-21 19:26:32 -0300

    rain

---
+++ canvas/Rain.js
@@ -0,0 +1,64 @@
+class Rain extends MainProject {
+  constructor(canvas) {
+    super(canvas);
+
+    this.raindrops = this.generateRainDrops(200);
+
+    this.drawFrame();
+    this.showDisabled();
+    
+  }
+  
+  drawFrame() {
+    drawDarkBackground(this.ctx);
+    for ( let i = 0; i < this.raindrops.length; i++ ) {
+      console.log(this.raindrops[i]);
+      this.raindrops[i].update();
+      this.raindrops[i].draw(this.ctx);
+    }
+  }
+
+  generateRainDrops(N) {
+    let arr = [];
+
+    for ( let i = 0; i < N; i++ ) {
+      arr.push(
+        new RainDrop(
+        [ Math.random() * CANVAS_SIZE, 
+          Math.random() * CANVAS_SIZE ]));
+    }
+
+    return arr;
+  }
+}
+
+class RainDrop {
+  constructor(location) {
+    this.location = location;
+    this.oldLocation = location;
+    this.radius = Math.random() + 1.2;
+    this.speed = Math.random() * 50 + 50;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.lineWidth = this.radius;
+    ctx.strokeStyle = "rgba(255, 255, 255, 0.3)";
+    ctx.moveTo(...this.oldLocation);
+    ctx.lineTo(...this.location);
+    ctx.stroke();
+  }
+
+  update() {
+    this.oldLocation = [ this.location[0], this.location[1] ];
+    this.location[1] += this.speed;
+    
+    // if ( this.location[1] > CANVAS_SIZE ) {
+    //   this.location[1] -= CANVAS_SIZE;
+    //   this.oldLocation[1] -= CANVAS_SIZE;
+    // }
+
+    this.location[1] > CANVAS_SIZE ? (this.location[1] -= CANVAS_SIZE, this.oldLocation[1] -= CANVAS_SIZE) : null;    
+  }
+}
+
