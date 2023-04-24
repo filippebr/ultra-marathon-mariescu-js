9cc514925ad771ec1bf8cd605ed1d04117da29ab
Filippe Braga <filippeffx@hotmail.com>
2023-04-24 13:08:29 -0300

    initial cloud

--- canvas/cloud.js
+++ canvas/cloud.js
@@ -1,34 +1,40 @@
-class Fire extends MainProject {
+class Cloud extends MainProject {
   constructor(canvas) {
     super(canvas);
 
     // this.drawDarkBackground();
     drawDarkBackground(this.ctx);
-
-    this.fire = [];
+    this.cloudParts = this.generateCloudParts(100);
 
     this.drawFrame();
     this.showDisabled();
     
   }
-  
-  drawFrame() {
-    
-    this.fire.push( new Flame ([ 0.5 * CANVAS_SIZE, 0.75 * CANVAS_SIZE ]));
-    drawDarkBackground(this.ctx);
 
-    this.ctx.globalCompositeOperation = "lighter";
+  generateCloudParts(N) {
+    let arr = [];
 
-    for ( let f of this.fire ) {
-      f.step < f.lifespan && (f.update(), f.draw(this.ctx));
-      f.step >= f.lifespan && (this.fire.splice(this.fire.indexOf(f), 1), i--);
+    for (let i = 0; i < N; i++ ) {
+      arr.push(new CloudParticle([Math.random() * CANVAS_SIZE, CANVAS_SIZE / 2]))
     }
 
-    this.ctx.globalCompositeOperation = "source-atop";
+    return arr;
+  }
+  
+  drawFrame() {    
+    drawDarkBackground(this.ctx);
+
+    // this.ctx.globalCompositeOperation = "lighter";
+
+    for ( let cloud of this.cloudParts ) {
+      cloud.draw(this.ctx);
+    }
+
+    // this.ctx.globalCompositeOperation = "source-atop";
   }
 }
 
-class Flame {
+class CloudParticle {
   constructor(location) {
     this.location = location;
     this.radius = Math.random() * 10 + 50;
@@ -48,8 +54,8 @@ class Flame {
       this.location[0],
       this.location[1],
       this.radius);
-    grd.addColorStop(0, "rgba(255, " + this.green + ", 0, 1)");
-    grd.addColorStop(1, "rgba(255, " + this.green + ", 0, 0)");
+    grd.addColorStop(0, "rgba(255, 255, 255, 0.5)");
+    grd.addColorStop(1, "rgba(255, 255, 255, 0)");
     
     ctx.fillStyle = grd;
     ctx.arc(this.location[0], this.location[1], this.radius, 0, Math.PI * 2);
